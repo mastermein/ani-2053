f9b55c5cb2ac4364495a3e8298d137fbfd346888
Mastermein <Kengnebrayan3@gmail.com>
2026-09-18 22:04:09 +0200

    initialisation de main.cpp

---
+++ chapitre-02/exo4-le_commit_partiel/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Configuration de la vitesse
+    float vitesseJoueur = 100.0f;
+    
+    // Affichage des informations
+    std::cout << "Vitesse actuelle du joueur : " << vitesseJoueur << std::endl;
+    std::cout << "Bienvenue dans le jeu !" << std::endl;
+
+    return 0;
+}
